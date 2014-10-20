349dc2dc08e841765e9751b6f8d1c71143137f07
Kevin Witt <kjwitt87@gmail.com>
2014-10-20 01:01:53 -0400

    added successful serial arduino python script

--- Raspi/Development/serial_test.py
+++ Raspi/Development/serial_test.py
@@ -1,1 +1,18 @@
-import time
+import serial
+
+arduino = serial.Serial("/dev/ttyACM0",9600)
+
+state = 0
+while state !=9:
+	state=input("Enter a LED state (9 to quit): ")
+	if state == 0:
+		arduino.write('0')
+		print("Waiting for reply")
+		print(arduino.readline())
+	elif state == 1:
+		arduino.write('1')
+		print("Waiting for reply")
+		print(arduino.readline())
+	else:
+		print("Exiting")	
+	
